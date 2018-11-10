d64f3def4de967e97aad9729b408379cda71d2ca
Cuiods <cuihaoemail@163.com>
2018-11-10 11:32:27 +0800

    Add UI Design

---
+++ src/main/java/com/cuiods/cryptology/rsa/controller/RSAController.java
@@ -0,0 +1,13 @@
+package com.cuiods.cryptology.rsa.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RSAController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
--- src/main/java/com/cuiods/cryptology/rsa/main/Main.java
+++ src/main/java/com/cuiods/cryptology/rsa/main/Main.java
@@ -1,4 +1,32 @@
 package com.cuiods.cryptology.rsa.main;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            URL mainFxml = Main.class.getClassLoader().getResource("main.fxml");
+            if (mainFxml == null) return;
+            Parent root = FXMLLoader.load(mainFxml);
+            primaryStage.setTitle("RSA Tool");
+            primaryStage.setScene(new Scene(root));
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
